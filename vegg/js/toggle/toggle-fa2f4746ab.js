4f0d3bf352deb1bd4de2451dbc064f9421a90a7f
roobyroid <roobyroid7@gmail.com>
2019-06-19 00:16:32 +0300

    ree

--- vegg/js/toggle/toggle-fa2f4746ab.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$('.header__toggle').click(function(){
-		$('.header__toggle').toggleClass('active');
-	});
-	$('.header__toggle-list-link').click(function(){
-		$('.header__toggle').removeClass('active');
-	});
-	});
