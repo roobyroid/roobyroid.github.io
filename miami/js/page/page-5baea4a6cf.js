0395e31c3fbaff0f477119cf50bdd61ce5bfc259
roobyroid <roobyroid7@gmail.com>
2019-09-19 17:54:07 +0300

    miami

---
+++ miami/js/page/page-5baea4a6cf.js
@@ -0,0 +1,17 @@
+$(function () {
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+
+        $("html,body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+});
