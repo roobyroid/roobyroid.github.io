5c6641b09287cef250407fb1500752e1033b4ce0
roobyroid <roobyroid7@gmail.com>
2019-11-26 21:24:28 +0300

    dreams

---
+++ dreams/js/stocks/stocks-793023287b.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container.stocks__slider', {
+    slidesPerView: 1,
+    speed: 800,
+    navigation: {
+        nextEl: '.swiper-container.stocks__slider .swiper-button-next',
+        prevEl: '.swiper-container.stocks__slider .swiper-button-prev',
+    },
+});
